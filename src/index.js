50712d2617e08d2e2c746e4fcb02106d6705c6ac
cfl014 <cfl.404.bsso@gmail.com>
2023-09-22 20:43:33 -0300

    la rompimos toda pa(controller y queri)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express, { json, urlencoded } from "express";
+import cors from "cors"
+import nuevoDocentesRouter from "./routes/alta-docente.routes.js";
 const app = express();
 const port = 8080;
 
+app.use(cors())
 app.use(json());
 app.use(urlencoded());
+app.use(nuevoDocentesRouter)
 app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+    console.log(`Servidor escuchando en el puerto ${port}`);
 });
