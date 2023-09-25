5a618e4601b3b30eb5988fce70b057d72daacf27
LeandroWolfLv <leandrovelazquez015@gmail.com>
2023-09-25 17:46:20 -0300

    primera subida

---
+++ src/database/queries.database.js
@@ -0,0 +1,9 @@
+export const selectCurso =`SELECT nombre FROM cursos`;
+
+export const buscarAlumno =`SELECT AL.tipo_dni,AL.nro_legajo, AL.nombre,AL.apellido, DAL.id_alumno,DAL.direccion, DAL.localidad,DAL.car_telefono, DAL.telefono, DAL.car_tel_extra, DAL.telefono_extra, DAL.email, DAL.fotoc_dni, DAL.fotoc_analitico, DAL.planilla_ins FROM alumnos AL INNER JOIN detalle_alumnos DAL ON DAL.id_alumno = AL.id_alumno WHERE AL.nro_dni = ?`; 
+
+//export const buscarDetalleAlumno =`SELECT direccion, localidad,car_telefono, telefono, car_tel_extra, telefono_extra, email, fotoc_dni, fotoc_analitico, planilla_ins FROM detalle_alumnos`
+
+export const insertAlumno = `INSERT INTO alumnos (tipo_dni,nro_dni,nro_legajo,nombre,apellido) VALUES (?,?,?,?,?)`;
+
+export const insertDetalleAlumno = `INSERT INTO detalle_alumnos (id_alumno,direccion, localidad,car_telefono, telefono, car_tel_extra, telefono_extra, email, fotoc_dni, fotoc_analitico, planilla_ins) VALUES (?,?,?,?,?,?,?,?,?,?,?)`
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express, { json, urlencoded } from "express";
+import cors from "cors";
+import nuevoAlumnoRouter from "./routes/alta-alumno.routes.js";
 const app = express();
+
 const port = 8080;
 
 app.use(json());
+
+app.use(cors());
+
 app.use(urlencoded());
+
+app.use(nuevoAlumnoRouter);
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
