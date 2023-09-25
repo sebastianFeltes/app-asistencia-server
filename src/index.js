fc9b443748280d0d0cb0b81fb7b8cf32e2cbd8ed
BossiKeesJuan <juam.boke5@gmail.com>
2023-09-25 20:45:16 -0300

    controladores y sessiones

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import asistenciaRouter from "./routes/asistencia-alumnos.routes.js";
+import session from "express-session";
+
 
 const app = express();
 const port = 8080;
@@ -13,3 +15,10 @@ app.use(asistenciaRouter)
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
+
+app.set("trust proxy", 1);
+app.use(session({
+    secret : " black-cat",
+    resave: true,
+    saveUninitialized: true
+}));
