15e6b5a3aab4102b8fab448070247caa702f473a
CFL-PC04 <cflpccerocuatro@gmail.com>
2023-10-02 20:42:58 -0300

    intento context

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,33 +3,45 @@ import { selectDocentePorDni } from "../database/queries.database.js";
 import bcrypt from "bcrypt";
 
 export function tryLogin(req, res) {
-	const { nro_dni, password } = req.body;
-	try {
-		db.all(selectDocentePorDni, [nro_dni], (err, rows) => {
-			if (err) {
-				console.log(err);
-				return res.json({ message: err.message }).status(500);
-			}
-			if (rows.lenght == 0) {
-				return res.json({ message: "dni inválido" }).status(400);
-			}
-			const dbPassword = rows[0].password;
+  const { nro_dni, password } = req.body;
+  console.log(req.body);
+  try {
+    db.all(selectDocentePorDni, [nro_dni], (err, rows) => {
+      if (err) {
+        console.log(err);
+        return res.json({ message: err.message }).status(500);
+      }
+      if (rows.lenght == 0) {
+        console.log(rows);
+        return res.json({ message: "dni inválido" }).status(400);
+      }
+      const dbPassword = rows[0].password;
 
-			const match = bcrypt.compare(password, dbPassword);
+      const match = bcrypt.compare(password, dbPassword);
 
-			if (match) {
-				req.session.docente_id = rows[0].docente_id;
-				req.session.docente_rol = rows[0].docente_rol;
-				req.session.docente_dni = rows[0].docente_dni;
-				req.session.docente_nombre = rows[0].docente_nombre;
-				req.session.docente_apellido = rows[0].docente_apellido;
-				console.log(req.session)
-				return res.json({ message: "loggeado" }).status(200);
-			} else {
-				return res.json({ message: "contraseña inválida!" }).status(400);
-			}
-		});
-	} catch (error) {
-		return json({ message: error.message }).status(500);
-	}
+      if (match) {
+        req.session.id_docente = rows[0].id_docente;
+        req.session.nombre = rows[0].nombre;
+        req.session.tipo_dni = rows[0].tipo_dni;
+        req.session.nro_dni = rows[0].nro_dni;
+        req.session.id_rol = rows[0].id_rol;
+        req.session.apellido = rows[0].apellido;
+
+        const userData = {
+          id_docente: rows[0].id_docente,
+          nombre: rows[0].nombre,
+          tipo_dni: rows[0].tipo_dni,
+          nro_dni: rows[0].nro_dni,
+          id_rol: rows[0].id_rol,
+          apellido: rows[0].apellido,
+        };
+        console.log(req.session);
+        return res.json(userData).status(200);
+      } else {
+        return res.json({ message: "contraseña inválida!" }).status(400);
+      }
+    });
+  } catch (error) {
+    return json({ message: error.message }).status(500);
+  }
 }
