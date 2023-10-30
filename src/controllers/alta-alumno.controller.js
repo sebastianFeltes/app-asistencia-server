32a0ee220a2a7a12f19952dc0d71ea30ea744a7f
LeandroWolfLv <leandrovelazquez015@gmail.com>
2023-10-30 20:26:54 -0300

    pedido y envio del campo fecha nac

--- src/controllers/alta-alumno.controller.js
+++ src/controllers/alta-alumno.controller.js
@@ -3,30 +3,29 @@ import {
   buscarAlumno,
   insertAlumno,
   insertDetalleAlumno,
-  insertIdRel,
   mostrarCursos,
 } from "../database/queries.database.js";
 
-
-//FUNCION INSERTAR UN NUEVO ALUMNO
+//TODO:FUNCION INSERTAR UN NUEVO ALUMNO
 export function tryAltaAlumno(req, res) {
-  console.log(req.body)
+  console.log(req.body);
   const {
-    tipoDoc,
-    dni,
-    numLegajo,
+    tipo_dni,
+    nro_dni,
+    nro_legajo,
     nombre,
     apellido,
     direccion,
     localidad,
-    telCar,
-    tel,
-    telCarExt,
-    telExt,
+    fecha_nac,
+    car_telefono,
+    telefono,
+    car_tel_extra,
+    telefono_extra,
     email,
-    documentacionDni,
-    documentacionAnalitico,
-    documentacionPlanilla,
+    fotoc_dni,
+    fotoc_analitico,
+    planilla_ins,
     cursos
   } = req.body;
   /*  const {docente_id, docente_rol} = req.body;
@@ -40,11 +39,12 @@ export function tryAltaAlumno(req, res) {
     
   }
  */
-  //DB (INSERTAR NUEVO ALUMNO)
+
+  //TODO:DB (INSERTAR NUEVO ALUMNO)
   try {
     db.run(
       insertAlumno,
-      [tipoDoc, dni, numLegajo, nombre, apellido],
+      [tipo_dni,nro_dni, nro_legajo, nombre, apellido,fecha_nac],
       (err, rows) => {
         if (err) {
           console.log(err.message);
@@ -59,22 +59,21 @@ export function tryAltaAlumno(req, res) {
             return console.log(err.message);
           }
           let alumno_id = rows[0].id;
-
-          //DB "detalle_alumnos"
+          //TODO:DB "detalle_alumnos"
           db.run(
             insertDetalleAlumno,
             [
               alumno_id,
               direccion,
               localidad,
-              telCar,
-              tel,
-              telCarExt,
-              telExt,
+              car_telefono,
+              telefono,
+              car_tel_extra,
+              telefono_extra,
               email,
-              documentacionDni,
-              documentacionAnalitico,
-              documentacionPlanilla,
+              fotoc_dni,
+              fotoc_analitico,
+              planilla_ins,
             ],
             (err, rows) => {
               if (err) {
@@ -83,32 +82,16 @@ export function tryAltaAlumno(req, res) {
                   .json({ mensaje: "No pudo insertar detalle del alumno" })
                   .status(400);
               }
-              /* db.all(
-                "SELECT MAX(id_relacion) FROM rel_curso_alumnos",
-                (err, rows) => {
-                  if (err) {
-                    return console.log(err.message);
+              cursos.map((e) => {
+                db.all(
+                  "INSERT INTO rel_curso_alumnos (id_alumno, id_curso) VALUES (?,?)",
+                  [alumno_id, e],
+                  (err, rows) => {
+                    if (err) res.json({ mensaje: err.message });
                   }
-                  let relacion_id = rows[0].id_relacion;
+                );
+              });
 
-                  db.run(insertIdRel, [relacion_id, alumno_id,], (err, rows) => {
-                    if (err) {
-                      console.log(err.message);
-                      return res.json({
-                          mensaje: "No pudo insertar relacion",
-                        })
-                        .status(400);
-                    }
-                  });
-                }
-              ); */
-              cursos.map(e=>{
-                db.all("INSERT INTO rel_curso_alumnos (id_alumno, id_curso) VALUES (?,?)", [alumno_id, e],(err,rows)=>{
-                  if(err)res.json({mensaje: err.message})
-  
-                })
-              })
-              
               return res.json("recibido").status(200);
             }
           );
@@ -116,11 +99,11 @@ export function tryAltaAlumno(req, res) {
       }
     );
   } catch (error) {
-    return res.json(rows).status(500);
+    return res.json({ mensaje: "error en el servidor" }).status(500);
   }
 }
 
-//FUNCION BUSCAR ALUMNOS
+//TODO:FUNCION BUSCAR ALUMNOS CON EL nro_dni
 export function traerAlumno(req, res) {
   const nro_dni = req.params.nro_dni;
   try {
@@ -129,7 +112,6 @@ export function traerAlumno(req, res) {
         console.log(err.message);
         return res.json({ mensaje: err.message }).status(500);
       }
-      console.log(rows[0]);
       return res.json(rows[0]).status(200);
     });
   } catch (error) {
@@ -137,7 +119,7 @@ export function traerAlumno(req, res) {
   }
 }
 
-//FUNCION TRAER CURSOS
+//TODO:FUNCION TRAER CURSOS
 
 export function buscarCurso(req, res) {
   try {
