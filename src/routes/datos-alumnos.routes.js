4743346c9d920fe7d46c7cae520c0e6e40026506
cfl404pc02 <cfl404pc02@gmail.com>
2023-11-06 20:40:18 -0300

    historial alumnos

--- src/routes/datos-alumnos.routes.js
+++ src/routes/datos-alumnos.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { datosAlumnos, modificarDatosAlumno } from "../controllers/datos-alumnos.controller.js";
 import { alumnoValidator } from "../middlewares/alumnoValidator.middlewares.js";
-import { modicacionAlumnos } from "../schemas/modificacion-alumnos.schemas.js";
+import { modificacionAlumnos } from "../schemas/modificacion-alumnos.schemas.js";
 
 
 const alumnosRouter = Router();
 
 alumnosRouter.get("/datos-alumnos", datosAlumnos);
-alumnosRouter.post("/datos-alumnos",alumnoValidator(modicacionAlumnos), modificarDatosAlumno);
+alumnosRouter.post("/datos-alumnos",(req,res)=>{console.log(req.body)},alumnoValidator(modificacionAlumnos), modificarDatosAlumno);
 
 export default alumnosRouter;
 
