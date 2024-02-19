683f1dc15a01f04b37a1099c47d088c06be1aecf
SebastianFeltes <jikansui16@gmail.com>
2024-02-19 19:54:35 -0300

    fix controller queries

--- src/database/queries.database.js
+++ src/database/queries.database.js
@@ -24,7 +24,7 @@ export const insertAlumno = `INSERT INTO alumnos (tipo_dni,nro_dni,nro_legajo,no
 export const insertCurso = `INSERT INTO cursos (nombre ,id_docente, horario_inicio, horario_final,activo, fecha_inicio,fecha_final, horas_catedra,cantidad_clases) VALUES(?,?,?,?,?,?,?,?)`;
 export const insertRelCursoDia = `INSERT INTO rel_curso_dia (id_curso,id_dia) VALUES (?,?)`;
 export const eliminarRelCursoDia = `DELETE FROM rel_curso_dia WHERE id_curso = ?`;
-export const insertDetalleAlumno = `INSERT INTO detalle_alumnos (id_alumno,direccion, localidad,car_telefono, telefono, car_tel_extra, telefono_extra, email,lugar_nacimiento,nacionalidad, fotoc_dni, fotoc_analitico, planilla_ins ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+export const insertDetalleAlumno = `INSERT INTO detalle_alumnos (id_alumno,direccion, localidad,car_telefono, telefono, car_tel_extra, telefono_extra, email, lugar_nacimiento, nacionalidad, fotoc_dni, fotoc_analitico, planilla_ins ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`;
 
 export const updateAltaAlumnos = `UPDATE alumnos SET 
 tipo_dni=?, 
@@ -67,7 +67,7 @@ telefono=?,
 car_tel_extra=?, 
 telefono_extra=?, 
 email=?,
-lugar_nacimiento=?,
+lugar_nacimiento = ?,
 nacionalidad = ?, 
 fotoc_dni=?, 
 fotoc_analitico=?, 
@@ -88,7 +88,7 @@ export const selectDiasCursos = `SELECT * FROM rel_curso_dia RCD INNER JOIN dias
 
 export const selectDias = `SELECT * FROM dias DI`;
 
-export const updateCursos = `UPDATE cursos SET nombre=?, id_docente=?, horario_inicio=?, horario_final=?, activo=?, fecha_inicio=?, fecha_final=?, horas_catedra=? WHERE id_curso=?`;
+export const updateCursos = `UPDATE cursos SET nombre=?, id_docente=?, horario_inicio=?, horario_final=?, activo=?, fecha_inicio=?, fecha_final=?, horas_catedra=?, cantidad_clases = ? WHERE id_curso=?`;
 export const updatetAsistAlumno =
   "UPDATE asistencia SET cod_asistencia = ? WHERE id_asistencia = ?";
 
